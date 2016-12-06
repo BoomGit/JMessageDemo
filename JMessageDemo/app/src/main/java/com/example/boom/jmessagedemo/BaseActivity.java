8e36ae6ac383b2ff222f4877507e4713cbfed450
Boom <631903189@qq.com>
2016-12-06 22:42:54 +0800

    添加项目文件

---
+++ JMessageDemo/app/src/main/java/com/example/boom/jmessagedemo/BaseActivity.java
@@ -0,0 +1,20 @@
+package com.example.boom.jmessagedemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Boom on 2016/12/5.
+ */
+
+public class BaseActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+
+
+}
---
+++ JMessageDemo/app/src/main/java/com/example/boom/jmessagedemo/application/MyApp.java
@@ -0,0 +1,16 @@
+package com.example.boom.jmessagedemo.application;
+
+import android.app.Application;
+
+/**
+ * Created by Boom on 2016/12/5.
+ */
+
+public class MyApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+}
